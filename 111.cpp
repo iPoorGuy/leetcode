c07423ec586197f79196740b884db37948f8c225
sun <1714102764@qq.com>
2017-02-28 21:36:11 +0800

    tree广度遍历

--- 111.cpp
+++ 111.cpp
@@ -8,12 +8,18 @@ struct TreeNode {
   TreeNode *right;
   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+/**
+ * 主要是树的广度遍历。
+ * 给出的结构中没有parent指针，不能用深度遍历!
+ */
 
 class Solution {
   public:
       int minDepth(TreeNode* root) {
-        if(root->left == NULL && root->right == NULL)
-          return 1;
+//        if(root->left == NULL && root->right == NULL)
+//          return 1;
+        if(root == NULL)
+          return 0;
         vector<TreeNode*> tree_ptr;
         int deep = 1, offset = 0;
         tree_ptr.push_back(root);
